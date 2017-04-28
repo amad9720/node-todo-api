a657103a54646b4db6c91ce8746be0f7ae979885
Amadou Ly <amly212782@gmail.com>
2017-04-28 20:04:49 +0200

    Add DELETE /todos/:id root

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,5 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todos');
+const {User} = require('./../server/models/user');
